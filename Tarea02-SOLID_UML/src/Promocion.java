6f7827e7327eb4166d5c8368da8277af92633392
MoreiraC18 <cesmchav@espol.edu.ec>
2025-06-20 14:00:54 -0500

    Create class Promocion

---
+++ Tarea02-SOLID_UML/src/Promocion.java
@@ -0,0 +1,11 @@
+public class Promocion {
+    private String descripcion;
+    private float descuenta;
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public float getDescuenta() {
+        return descuenta;
+    }
+}
